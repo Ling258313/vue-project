e0c6f4b0ed4da7e1d6ddf11de328ecec0b2de1f8
ZO永远的神 <1273125192@qq.com>
2026-08-30 01:36:55 +0800

    feat: 配置应用入口（路由/pinia/全局组件/样式挂载）

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 // 注册 SVG 图标
 import 'virtual:svg-icons-register'
-import './style.css'
+// 引入自定义插件对象：注册全局组件
+import globalComponents from '@/components'
 import App from '@/App.vue'
-
+//引用路由
+import router from './router'
+//引用仓库
+import pinia from './store'
 // 获取应用实例对象
 const app = createApp(App)
 
@@ -16,5 +20,13 @@ const app = createApp(App)
 app.use(ElementPlus, {
   locale: zhCn, // element-plus 国际化配置
 })
+//引入模板的全局样式
+import '@/styles/index.scss'
+// 注册全局自定义组件
+app.use(globalComponents)
+//安装仓库
+app.use(pinia)
+// 注册模板路由
+app.use(router)
 // 将应用挂载到挂载点上
 app.mount('#app')
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,13 @@ declare module 'virtual:svg-icons-register' {
   const src: string
   export default src
 }
+
+// 自定义环境变量类型声明
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_BASE_API: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
